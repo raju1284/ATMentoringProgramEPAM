6333934374cc07c8a6270df48ebbd5302a9013ab
Nagaraju_Dasari <nagaraju_dasari@epam.com>
2023-10-25 15:00:09 -0500

    Updated Basetest and testng runner file for refactoring

--- src/test/java/components/ui/BaseTest.java
+++ src/test/java/components/ui/BaseTest.java
@@ -1,7 +1,9 @@
 package components.ui;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
@@ -9,6 +11,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.event.EventRecodingLogger;
@@ -16,6 +20,7 @@ import utilities.readPropertyFile;
 
 
 import java.io.IOException;
+import java.time.Duration;
 
 import static org.openqa.selenium.remote.CapabilityType.PROXY;
 
@@ -24,46 +29,41 @@ public class BaseTest {
     readPropertyFile fr = new readPropertyFile();
     private final static Logger logger = LoggerFactory.getLogger(BaseTest.class);
 
-    public WebDriver getDriver() throws IOException {
-
-        String browser =  fr.getPropertyValue("browser");
-
-            if (browser.equalsIgnoreCase("edge"))
-        {
-          try {
-              System.setProperty("webdriver.edge.driver",
-                      System.getProperty("user.dir") + "\\" + "src\\test\\resources\\drivers\\msedgedriver.exe");
-
-              EdgeOptions options = new EdgeOptions();
-              options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-              options.addArguments("--remote-allow-origins=*");
-              options.setCapability("ignore-certificate-errors", true);
-
-              WebDriverManager.edgedriver().avoidResolutionCache().proxy(PROXY).setup();
-              driver = new EdgeDriver(options);
-          }
-          catch (Exception exp){
-              exp.printStackTrace();
-          }
-        }
-      return driver;
+    public void BaseTest(WebDriver driver)
+    {
+        this.driver = driver;
     }
 
-    public void navigateToUrl(String url)
+
+    public void navigateToUrl(WebDriver driver,String url)
     {
         driver.get(url);
         logger.info("open the Report portal Successful");
     }
 
-    public String getTitle()
+    public String getPageTitle(WebDriver driver)
     {
         return driver.getTitle();
     }
 
-    public void driverClose()
+    public void driverClose(WebDriver driver)
     {
         driver.close();
         logger.info("Webdriver Closed successfully");
 
+    }
+    public void doClick(WebDriver driver, String locator) {
+       WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+       WebElement elementToClick ;
+       elementToClick = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+       elementToClick.click();
+
+    }
+
+    public void sendKeys(WebDriver driver, String locator, String text) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement elementToSendKeys ;
+        elementToSendKeys = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+        elementToSendKeys.sendKeys(text);
     }
 }
