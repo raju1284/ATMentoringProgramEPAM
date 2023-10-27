70e44730f45129bae9bf34e5d96a84d211bc51fb
Nagaraju_Dasari <nagaraju_dasari@epam.com>
2023-10-26 19:15:17 -0500

    Added Chrome driver

--- src/test/java/components/ui/BaseTest.java
+++ src/test/java/components/ui/BaseTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import utilities.ReadPropertyFile;
 
 import java.io.File;
@@ -32,15 +33,15 @@ public class BaseTest  {
         if (browser.equalsIgnoreCase("edge"))
         {
             try {
-                String[] pathNames = {absolutePath, "\\msedgedriver.exe" };
+                String[] pathNames = {absolutePath, "\\drivers\\msedgedriver.exe" };
                 String path = String.join(File.pathSeparator, pathNames);
-                System.setProperty("webdriver.edge.driver",path);
+                System.setProperty("webdriver.edge.driver",path.replaceAll(";",""));
                 EdgeOptions options = new EdgeOptions();
                 options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
                 options.addArguments("--remote-allow-origins=*");
                 options.setCapability("ignore-certificate-errors", true);
 
-                WebDriverManager.edgedriver().avoidResolutionCache().proxy(PROXY).setup();
+
                 driver = new EdgeDriver(options);
             }
             catch (Exception exp){
@@ -48,15 +49,16 @@ public class BaseTest  {
             }
         }
         else if(browser.equalsIgnoreCase("chrome")) {
-            String[] pathNames = {absolutePath, "\\chromedriver.exe" };
+            String[] pathNames = {absolutePath, "\\drivers\\chromedriver.exe" };
             String path = String.join(File.pathSeparator, pathNames);
-            System.setProperty("webdriver.chrome.driver",path);
+            System.out.println(path);
+            System.setProperty("webdriver.chrome.driver",path.replaceAll(";",""));
             ChromeOptions options = new ChromeOptions();
             options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
             options.addArguments("--remote-allow-origins=*");
             options.setCapability("ignore-certificate-errors", true);
 
-            WebDriverManager.chromedriver().avoidResolutionCache().proxy(PROXY).setup();
+
             driver = new ChromeDriver(options);
         }
         return driver;
