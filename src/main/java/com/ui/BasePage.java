2ce7562b160007837b05cefebbd8728a847f4151
Nagaraju_Dasari <nagaraju_dasari@epam.com>
2023-10-28 00:33:41 -0500

    Clean up the code as per review comments mentioned in PR

--- src/main/java/com/ui/BasePage.java
+++ src/main/java/com/ui/BasePage.java
@@ -1,10 +1,12 @@
 package com.ui;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,12 +19,18 @@ public class BasePage {
     protected WebDriver driver;
 
     private final static Logger logger = LoggerFactory.getLogger(BasePage.class);
-    private WebDriverWait wait;
+    private static WebDriverWait wait;
     ///Initializing the Web driver and wait time////
     public BasePage(WebDriver driver)
     {
         this.driver = driver;
-        this.wait = new WebDriverWait(this.driver, Duration.ofSeconds(10));
+
+    }
+
+    public static void waitForElement(WebDriver driver, WebElement element) {
+        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
     /// This method will perform the action of navigate to specific given url///
     public void navigateToUrl(String url)
@@ -38,12 +46,14 @@ public class BasePage {
 
 /// This method will perform the action of Webelements Click///
     public void doClick( String locator) {
-        WebElement elementToClick = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
-        elementToClick.click();
+        waitForElement(driver, driver.findElement(By.xpath(locator)));
+        WebElement elementToCLick= driver.findElement(By.xpath(locator));
+        elementToCLick.click();
     }
     /// This method will perform the action of sendkeys to a textbox Webelements///
     public void sendKeys(String locator, String text) {
-        WebElement elementToSendKeys = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+        waitForElement(driver, driver.findElement(By.xpath(locator)));
+        WebElement elementToSendKeys = driver.findElement(By.xpath(locator));
         elementToSendKeys.sendKeys(text);
     }
 }
