ff2053d2adfd2c3472a57787f67a3a8c868ed6c3
Nagaraju_Dasari <nagaraju_dasari@epam.com>
2023-10-28 00:12:47 -0500

    Clean up the code as per review comments mentioned in PR

--- src/main/java/com/ui/BasePage.java
+++ src/main/java/com/ui/BasePage.java
@@ -22,7 +22,7 @@ public class BasePage {
     public BasePage(WebDriver driver)
     {
         this.driver = driver;
-        wait = new WebDriverWait(this.driver, Duration.ofSeconds(10));
+        this.wait = new WebDriverWait(this.driver, Duration.ofSeconds(10));
     }
     /// This method will perform the action of navigate to specific given url///
     public void navigateToUrl(String url)
